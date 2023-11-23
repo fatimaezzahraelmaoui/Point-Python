268e57aa7989b9bb1e07858c8f7979a40947d413
fatimaezzahraelmaoui <148896755+fatimaezzahraelmaoui@users.noreply.github.com>
2023-11-23 23:41:25 +0100

    Add files via upload

---
+++ Point.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def getx(self):
+        return self.x
+    def setx(self,x):
+        self.x = x
+    def gety(self):
+        return self.y 
+    def sety(self):
+        self.y = y
+    def distance(self,Point=2):
+        return (( self.Point)*2 + ( self.Point)*2)*(1/2)
+    def NORM(self):
+        N =(self.x *2 + self.y *2)*(1/2)
+        return N
+P1 = Point(2,4)
+distance = P1.distance()
+print(P1.distance)
